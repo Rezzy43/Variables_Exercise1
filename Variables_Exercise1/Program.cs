a9a41cc3896195ac5175f325e59b585718d7e8c4
Michael Resendez <michael.resendez1@hotmail.com>
2024-10-02 16:48:00 -0500

    first commit

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Variables_Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring and Initializing Variables
+            string name = "Michael";
+            int age = 43;
+            char middleInitial = 'J';
+            bool isOver18 = true;
+            double currentTemp = 91.2;
+            decimal currentPrice = 29.99m;
+
+            // Writing the variables to the Console
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
+        }
+    }
+}
